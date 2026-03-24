9897f9bc8d7f49b3a6377c58643bcdb90d40ac62
Reyzikuuu <virmanadipuraaaa@gmail.com>
2026-03-24 15:00:12 +0700

    config: remove base path for Cloudflare Pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-    base: '/wift-lpwa/', // GitHub Pages repo name
 
     plugins: [
         tailwindcss(),
